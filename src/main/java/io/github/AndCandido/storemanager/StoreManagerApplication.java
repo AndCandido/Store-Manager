d0d2c33810773fcc425af2978ab014ccf2203ccf
André <100149329+AndCandido@users.noreply.github.com>
2023-10-12 19:58:09 -0300

    Initial project structure

---
+++ src/main/java/io/github/AndCandido/storemanager/StoreManagerApplication.java
@@ -0,0 +1,12 @@
+package io.github.AndCandido.storemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/AndCandido/storemanager/StoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AndCandido.storemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
