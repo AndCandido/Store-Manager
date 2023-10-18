1463485c7a8339629df65ece154e2ed8f0b5bbd1
André <100149329+AndCandido@users.noreply.github.com>
2023-10-17 21:19:10 -0300

    Modified enum PaymentMethod

--- src/main/java/io/github/AndCandido/storemanager/domain/enums/PaymentMethod.java
+++ src/main/java/io/github/AndCandido/storemanager/domain/enums/PaymentMethod.java
@@ -1,10 +1,35 @@
 package io.github.AndCandido.storemanager.domain.enums;
 
-public enum PaymentMethod {
-        MONEY,
-        DEBIT_CARD,
-        CREDIT_CARD,
-        PIX,
-        NEGOTIATED
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
+public enum PaymentMethod {
+    MONEY("money"),
+    DEBIT_CARD("debit_card"),
+    CREDIT_CARD("credit_card"),
+    PIX("pix"),
+    NEGOTIATED("negotiated");
+
+    private String value;
+
+    PaymentMethod(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+
+
+//    @JsonCreator
+//    public static PaymentMethod fromString(String value) {
+//        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+//            if (paymentMethod.name().equalsIgnoreCase(value)) {
+//                return paymentMethod;
+//            }
+//        }
+//        throw new IllegalArgumentException("Método de pagamento inválido");
+//    }
 }
--- src/main/java/io/github/AndCandido/storemanager/web/advice/HandlerExceptionsController.java
+++ src/main/java/io/github/AndCandido/storemanager/web/advice/HandlerExceptionsController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
@@ -30,4 +31,9 @@ public class HandlerExceptionsController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handlerIllegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
+    }
 }
