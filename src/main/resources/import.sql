bbf42e1663da30f7f80510e233b6ae943e825125
André <100149329+AndCandido@users.noreply.github.com>
2024-02-21 15:06:51 -0300

    feat: Add admin user in database for development

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO TB_PRODUCTS (ID, NAME, PRICE , STOCK_QUANTITY, CREATED_AT) VALUES (1, 'Calça Masc', 149.99, 5, now()), (2, 'Bermuda Juv', 70, 10, now()), (3, 'Saia Jeans', 110.05, 1, now()), (4, 'Carrinho de Brinquedo', 19.90, 15, now()), (5, 'Toalha', 22.5, 25, now());
-INSERT INTO TB_CUSTOMERS (ID, NAME, NICKNAME, CPF, ADDRESS, PHONE, CREATED_AT) VALUES (random_uuid(), 'Alberto Lima Castro', NULL, '021.419.780-84', 'Rua. Av. Bairro Santarém', NULL, now())
+INSERT INTO TB_CUSTOMERS (ID, NAME, NICKNAME, CPF, ADDRESS, PHONE, CREATED_AT) VALUES ('b7473980-d54c-49a8-8412-986ac5e659ad', 'Alberto Lima Castro', NULL, '021.419.780-84', 'Rua. Av. Bairro Santarém', NULL, now())
+INSERT INTO TB_USERS (ID, ROLE, PASSWORD, USERNAME) VALUES (random_uuid(), 0, '$2a$10$Plew4urXjngsHiuI4rCen.WvBWI6l/TwGwaFngEUVDICP6hRWlw1G', 'and')
